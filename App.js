6c7fa338c5d9cde9f190c5ea76924f908686ea13
kalvinbenser <kalvinbenser1@gmail.com>
2022-08-21 23:40:10 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import All from './src/screens/All';
+import Business from './src/screens/Business';
+import Health from './src/screens/Health';
+import Sports from './src/screens/Sports';
+import Tech from './src/screens/Tech';
+import { Icon } from 'react-native-elements';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="All" component={All}  options={{
+            tabBarIcon: (props) => (
+              <Icon type='feather' name='home' color={props.color} />
+            ),
+          }} />
+      <Tab.Screen name="Business" component={Business} 
+      options={{
+        tabBarIcon: (props) => (
+          <Icon type='feather' name='dollar-sign' color={props.color} />
+        ),
+      }} />
+      <Tab.Screen name="Health" component={Health} 
+      options={{
+        tabBarIcon: (props) => (
+          <Icon type='feather' name='heart' color={props.color} />
+        ),
+      }} 
+      />
+      <Tab.Screen name="Sports" component={Sports} 
+       options={{
+        tabBarIcon: (props) => (
+          <Icon type='ionicon' name="tennisball-outline" color={props.color} />
+        ),
+      }}
+      />
+      <Tab.Screen name="Tech" component={Tech} 
+       options={{
+        tabBarIcon: (props) => (
+          <Icon type='ionicon' name="hardware-chip-outline" color={props.color} />
+        ),
+      }} 
+      />
+    </Tab.Navigator>
+  </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ src/screens/Tech.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import { NativeBaseProvider,  ScrollView, Divider, Image, Spinner } from 'native-base';
+import { services } from '../../services/services';
+import moment from 'moment'
+const Tech = () => {
+    const [newsData, setNewsData] = useState([])
+    useEffect(() => {
+        services('technology')
+            .then(data => {
+                setNewsData(data)
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }, [])
+
+    return (
+        <NativeBaseProvider>
+            <ScrollView height={850}  >
+                {
+                    newsData.length > 1 ?
+                        newsData.map((item, index) =>
+
+                            <View key={index}>
+                                <View style={styles.newsContainer}>
+                                    <Image
+                                        width={550}
+                                        height={250}
+                                        resizeMode={"cover"}
+                                        source={{
+                                            uri: item.urlToImage,
+                                        }}
+                                        alt="Alternate Text"
+                                    />
+                                    <Text style={styles.title}>
+                                        {item.title}
+                                    </Text>
+                                    <Text style={styles.date}>
+                                        {moment(item.publishedAt).format('LLL')}
+                                    </Text>
+                                    <Text style={styles.newsDescription}>
+                                        {item.description}
+                                    </Text>
+                                </View>
+                                <Divider my={2} bg="#e0e0e0" />
+                            </View>
+                        ) : <View style={styles.spinner}>
+                            <Spinner color="danger.400" />
+                        </View>
+                }
+
+            </ScrollView>
+        </NativeBaseProvider>
+    )
+}
+
+export default Tech
+
+const styles = StyleSheet.create({
+    newsContainer: {
+        padding: 10
+    },
+    title: {
+        fontSize: 18,
+        marginTop: 10,
+        fontWeight: "600"
+    },
+    newsDescription: {
+        fontSize: 16,
+        marginTop: 10
+    },
+    date: {
+        fontSize: 14
+    },
+    spinner: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 400
+    }
+});
